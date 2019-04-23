c22bbecc6869510c5ea3eda91be0bc704195412c
liujiazhen <liujiazhen@live.cn>
2019-04-23 12:42:12 +0800

    ADD: 增加SpringBoot启动类

---
+++ bridge/src/main/java/com/liujiazhen/study/bridge/BridgeApplication.java
@@ -0,0 +1,26 @@
+package com.liujiazhen.study.bridge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Copyright © 2019 LiuJiazhen
+ * <p>
+ * description:
+ *
+ * @author LiuJiazhen
+ * @version v1.0.0
+ * @date 2019/4/23 10:13
+ * <p>
+ * Modification History
+ * Date     Author      Version     Description
+ * ---------------------------------------------------------*
+ * 2019/4/23      LJZ     v1.0.0      create
+ */
+
+@SpringBootApplication
+public class BridgeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BridgeApplication.class, args);
+    }
+}
